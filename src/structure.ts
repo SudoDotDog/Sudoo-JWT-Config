ce8d9f31167c315a72e67b5b294f077f5335a3ba
WMXPY <wm@mengw.io>
2021-06-13 14:42:20 -0500

    add jwt structure

--- src/structure.ts
+++ src/structure.ts
@@ -6,10 +6,33 @@
 
 import { TokenTuple } from "./declare";
 
-export const deconstructJWT = (token: string): TokenTuple => {
+export const deconstructJWTOrNull = (token: string): TokenTuple | null => {
+
+    const tuple: TokenTuple = token.split('.') as any;
+
+    if (tuple.length !== 3) {
+        return null;
+    }
+    return tuple;
+};
+
+export const deconstructJWTOrThrow = (token: string, error?: Error): TokenTuple => {
+
+    const tuple: TokenTuple = token.split('.') as any;
+
+    if (tuple.length !== 3) {
+
+        if (error) {
+            throw error;
+        }
+        throw new Error("[Sudoo-JWT-Config] Invalid Token");
+    }
+    return tuple;
+};
+
+export const deconstructJWTEnsure = (token: string): TokenTuple => {
 
     const tuple: TokenTuple = token.split('.') as TokenTuple;
-
     return tuple;
 };
 
