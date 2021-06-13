b83aabecf4168ce7de2292d619b787ba699a6d9b
WMXPY <wm@mengw.io>
2021-06-13 14:24:45 -0500

    add jwt common util functions

---
+++ src/util.ts
@@ -0,0 +1,56 @@
+/**
+ * @author WMXPY
+ * @namespace JWTConfig
+ * @description Util
+ */
+
+export const convertJSTimeToUnixTime = (jsTime: number): number => {
+
+    const draftTime: number = jsTime / 1000;
+    const roundedTime: number = Math.floor(draftTime);
+
+    return roundedTime;
+};
+
+export const getCurrentUnixTime = (): number => {
+
+    return convertJSTimeToUnixTime(Date.now());
+};
+
+export const fixUndefinableDate = (target?: Date): number | undefined => {
+
+    if (typeof target === 'undefined') {
+        return undefined;
+    }
+
+    if (!target.getTime) {
+        return undefined;
+    }
+
+    const time: number = target.getTime();
+
+    if (isNaN(time)) {
+        return undefined;
+    }
+
+    return convertJSTimeToUnixTime(time);
+};
+
+export const fixNowDate = (target?: Date): number => {
+
+    if (typeof target === 'undefined') {
+        return getCurrentUnixTime();
+    }
+
+    if (!target.getTime) {
+        return getCurrentUnixTime();
+    }
+
+    const time: number = target.getTime();
+
+    if (isNaN(time)) {
+        return getCurrentUnixTime();
+    }
+
+    return convertJSTimeToUnixTime(time);
+};
