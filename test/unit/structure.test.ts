7e966fec7453586411d44458b0367d4de8df77df
WMXPY <wm@mengw.io>
2021-06-14 19:54:13 -0500

    improve struectue tests

--- test/unit/structure.test.ts
+++ test/unit/structure.test.ts
@@ -7,13 +7,26 @@
 
 import { expect } from 'chai';
 import * as Chance from 'chance';
+import { TokenTuple } from '../../src/declare';
+import { deconstructJWTOrNull } from '../../src/structure';
 
 describe('Given [Structure] Helper functions', (): void => {
 
     const chance: Chance.Chance = new Chance('jwt-config-structure');
 
-    it('placeholder', (): void => {
+    it('should be able to deconstruct JWT', (): void => {
 
-        expect(chance.string()).to.be.not.equal(chance.string());
+        const header: string = chance.string();
+        const body: string = chance.string();
+        const signature: string = chance.string();
+
+        const token: string = `${header}.${body}.${signature}`;
+        const tuple: TokenTuple | null = deconstructJWTOrNull(token);
+
+        expect(tuple).to.be.deep.equal([
+            header,
+            body,
+            signature,
+        ]);
     });
 });
